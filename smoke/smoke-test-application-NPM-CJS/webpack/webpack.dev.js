1dd4a31bc9e34c137d88c086f76c12808b8dddd9
Isha Joshi <ishajos@amazon.com>
2025-06-09 12:58:41 -0700

    fix: typo in loader file name

--- smoke/smoke-test-application-NPM-CJS/webpack/webpack.dev.js
+++ smoke/smoke-test-application-NPM-CJS/webpack/webpack.dev.js
@@ -25,7 +25,7 @@ module.exports = {
         loader_npm_rum_tmp: './src/loader-npm-rum-tmp.ts',
         loader_npm_rum_tmp_2: './src/loader-npm-rum-tmp-2.ts',
         loader_npm_rum_tmp_w3c_format_enabled:
-            './src/loader-npm-rum-w3c-format_enabled.ts'
+            './src/loader-npm-rum-w3c-format-enabled.ts'
     },
     resolve: {
         extensions: ['.ts', '.js', '.json'],
